61a4808922874b6f71ad3e9e94879485963ce12f
ismaelmaizon <ismael.maizon@gmail.com>
2023-11-17 13:47:02 -0300

    agregando estilos

--- frontend/src/components/Navbar/navbar.jsx
+++ frontend/src/components/Navbar/navbar.jsx
@@ -2,24 +2,43 @@ import React from "react";
 
 import axios from 'axios';
 import { useForm } from 'react-hook-form'
-import { Box, Center, Flex, Grid, GridItem, Icon, Input } from '@chakra-ui/react'
+import { Box, Center, Flex, FormControl, Grid, GridItem, Icon, IconButton, Input } from '@chakra-ui/react'
 import './navBar.css'
+import { SearchIcon } from '@chakra-ui/icons'
+
+
 
 export default function Navbar() {
 
+const {handleSubmit, register, getValues} = useForm()
+
+function onSubmit(values) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+        //alert(JSON.stringify(values, null, 2))
+        resolve()
+        }, 3000)
+    })
+}
+
+console.log(register());
+console.log(handleSubmit());
 
 return (
         <div className="navBar" >
             <Grid h='150px' w='100%' templateRows='repeat(20px, 10px)' alignItems={"center"}>
-                <GridItem w='100px' m='auto' >
-                    Ortopedia
+                <GridItem w='300px' m='auto' >
+                    Centro Pie Cordoba
                 </GridItem>
                 <GridItem w='100%'>
                     <Grid m='2px, 50px' templateColumns='repeat(3, 1fr)' gap={100} >
                         <GridItem display={"flex"} ml='40px' justifySelf={"initial"} >nombre secre/dr</GridItem>
-                        <GridItem display={"flex"} justifySelf={"center"} >
-                            <Input backgroundColor={"white"} placeholder="Buscar paciente" w='500px' ></Input>
-                        </GridItem>
+                        <form onSubmit={handleSubmit(onSubmit)}> 
+                            <FormControl display={"flex"} >
+                                <Input backgroundColor={"white"} placeholder="Buscar paciente" w='400px' type="text" {...register('valor')} ></Input>
+                                <IconButton aria-label='Search database' icon={<SearchIcon />} type='submit' onClick={()=> { console.log( getValues() );}}/>
+                            </FormControl>
+                        </form>
                         <GridItem display={"flex"} mr='40px' justifySelf={"end"} >fecha</GridItem>
                     </Grid>
                 </GridItem>
--- frontend/src/components/Secretariado/secretariado.jsx
+++ frontend/src/components/Secretariado/secretariado.jsx
@@ -6,36 +6,38 @@ import './secretariado.css'
 
 export default function Secretariado() {
 
+    const profesional = 'Profesional Asignado'
+
 return (
     <div className='secretariado'>
         <div className='secretariado_header'>
             <h2 className='secretariado_header_title' >Calendario dia de la fecha</h2>
-            <Button w='100%' mb='15px' >Asignar Turno</Button>
+            <Button w='100%' mb='15px'  >Asignar Turno</Button>
         </div>
-        <Grid w='90%' m='auto' mb='10px' p='12px' templateColumns='repeat(3, 1fr)' backgroundColor={'darksalmon'}>
+        <Grid w='90%' m='auto' mb='10px' p='12px' templateColumns='repeat(3, 1fr)' backgroundColor={'#00d4ff'} borderRadius="md">
                         <GridItem display={"flex"} justifySelf={"initial"} >fecha y hora</GridItem>
                         <GridItem display={"flex"} justifySelf={"center"} >datos paciente</GridItem>
-                        <GridItem display={"flex"} justifySelf={"end"} >Dr asignado</GridItem>
+                        <GridItem display={"flex"} justifySelf={"end"} >{profesional} </GridItem>
         </Grid>
-        <Grid w='90%' m='auto' mb='10px'  p='12px' templateColumns='repeat(3, 1fr)'backgroundColor={'darksalmon'}>
+        <Grid w='90%' m='auto' mb='10px' p='12px' templateColumns='repeat(3, 1fr)' backgroundColor={'#00d4ff'} borderRadius="md">
                         <GridItem display={"flex"} justifySelf={"initial"} >fecha y hora</GridItem>
                         <GridItem display={"flex"} justifySelf={"center"} >datos paciente</GridItem>
-                        <GridItem display={"flex"} justifySelf={"end"} >Dr asignado</GridItem>
+                        <GridItem display={"flex"} justifySelf={"end"} >{profesional}</GridItem>
         </Grid>
-        <Grid w='90%' m='auto' mb='10px' p='12px' templateColumns='repeat(3, 1fr)'backgroundColor={'darksalmon'}>
+        <Grid w='90%' m='auto' mb='10px' p='12px' templateColumns='repeat(3, 1fr)' backgroundColor={'#00d4ff'} borderRadius="md">
                         <GridItem display={"flex"} justifySelf={"initial"} >fecha y hora</GridItem>
                         <GridItem display={"flex"} justifySelf={"center"} >datos paciente</GridItem>
-                        <GridItem display={"flex"} justifySelf={"end"} >Dr asignado</GridItem>
+                        <GridItem display={"flex"} justifySelf={"end"} >{profesional}</GridItem>
         </Grid>
-        <Grid w='90%' m='auto' mb='10px' p='12px' templateColumns='repeat(3, 1fr)'backgroundColor={'darksalmon'}>
+        <Grid w='90%' m='auto' mb='10px' p='12px' templateColumns='repeat(3, 1fr)' backgroundColor={'#00d4ff'} borderRadius="md">
                         <GridItem display={"flex"} justifySelf={"initial"} >fecha y hora</GridItem>
                         <GridItem display={"flex"} justifySelf={"center"} >datos paciente</GridItem>
-                        <GridItem display={"flex"} justifySelf={"end"} >Dr asignado</GridItem>
+                        <GridItem display={"flex"} justifySelf={"end"} >{profesional}</GridItem>
         </Grid>
-        <Grid w='90%' m='auto' mb='10px' p='12px' templateColumns='repeat(3, 1fr)'backgroundColor={'darksalmon'}>
+        <Grid w='90%' m='auto' mb='10px' p='12px' templateColumns='repeat(3, 1fr)' backgroundColor={'#00d4ff'} borderRadius="md">
                         <GridItem display={"flex"} justifySelf={"initial"} >fecha y hora</GridItem>
                         <GridItem display={"flex"} justifySelf={"center"} >datos paciente</GridItem>
-                        <GridItem display={"flex"} justifySelf={"end"} >Dr asignado</GridItem>
+                        <GridItem display={"flex"} justifySelf={"end"} >{profesional}</GridItem>
         </Grid>
     </div>
 )
